21a5ca5e9101ba82f80a79a6c4521d36ca42245b
ABDULBASITKHAN001 <[basitkhurramkhan@gmail.com]>
2025-02-03 13:24:43 +0500

    JS-CHAP-03

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("HELLO-WORLD");
+
+// CHAPTER NUMBER 03: VARIABLES FOR NUMBERS 
+
+// QUESTION NUMBER 01: 
+var age = 18;
+alert("I am " + age + " years old");
+
+// QUESTION NUMBER 02:
+
+var visit = 39;
+alert("You have visited this site " + visit + " times");
+
+// QUESTION NUMBER 03:
+var birthYear = 2007;
+alert("MY BIRTH YEAR IS " + birthYear); // ALERT BIRTH YEAR! 
+ 
+// QUESTIOJN NUMBER 04:
+var visiName = "JHON DOE";
+var order = "5 T-Shirt(s)"
+
+document.write(visiName + " ordered " +  order + " on Variety Store ")
+
+
+
+///////////////////////// CHAPTER NUMBER 03: COMPLETED! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
